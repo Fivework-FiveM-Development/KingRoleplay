4e6217f60243bba103930d71951c3f2a04466c56
SergioBarrosDesenvolvimento <146868960+SergioBarrosDesenvolvimento@users.noreply.github.com>
2025-03-01 22:33:25 -0300

    nova base Mri-Qbox_V12949

---
+++ BaseMRI/Mri-Qbox_V12949/resources/[mri]/mri_Qbox/modules/mri/client-side/utilities-modules/_client.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{on("onResourceStart",resName=>{});})();
---
+++ BaseMRI/Mri-Qbox_V12949/resources/[mri]/mri_Qelevators/config.lua
@@ -0,0 +1,6 @@
+Lift = {}
+Lift.Data = require 'data.lift'
+Lift.Radial = 'OX'
+Lift.Debug = false
+Lift.QB = false
+Lift.UseRadial = false
