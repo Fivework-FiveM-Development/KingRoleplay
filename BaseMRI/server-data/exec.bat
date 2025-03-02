a966c87361fbfdf471a38f6cb3a6ce7702ac9091
SergioBarrosDesenvolvimento <146868960+SergioBarrosDesenvolvimento@users.noreply.github.com>
2025-03-01 23:11:10 -0300

    criado start

---
+++ BaseMRI/server-data/exec.bat
@@ -0,0 +1,15 @@
+@echo off
+echo "Iniciando a Base MRI-QBox King Roleplay"
+rmdir /s /q "cache"
+echo Cache deletado !
+
+@echo off
+@rem rmdir /s /q "source/cache"
+echo Cache deletado !
+pause 
+"../Server/FXServer.exe" +set serverProfile "default"
+echo "-------------------------------------------------"
+
+
+
+
