1781e38717b77c08d82781187a4860c47225306d
SergioBarrosDesenvolvimento <146868960+SergioBarrosDesenvolvimento@users.noreply.github.com>
2025-03-03 05:43:06 -0300

    mapas

---
+++ mapas/ScarfaceMansion/fxmanifest.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+fx_version 'cerulean'
+version '1.0.0'
+this_is_a_map 'yes'
+games { 'gta5' }
+
+lua54 'yes'
+
+
+'NO DUMP JAJAJA'
+
+
+--   Entre aqui para mais:
+-- discord.gg/ryzestore
+--   ryzestore.com
